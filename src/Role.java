b6632fa99a9776c71280f0b70675d9b615a27c45
fluke <flukebevil@gmail.com>
2017-05-09 22:16:33 +0700

    add Role clas

---
+++ src/Role.java
@@ -0,0 +1,18 @@
+
+public class Role {
+	
+	public String GetUser(){
+		return getUser()+"";
+	}
+	public String GetDoctor() {
+		return getDoc()+"";
+	}
+	
+	public int getUser(){
+		return 1;
+	}
+	public int getDoc() {
+		return 2;
+	}
+	
+}
