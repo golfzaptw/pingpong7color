0efa7087bbf008f96dceac6b071b8264070cf6e8
fluke <flukebevil@gmail.com>
2017-05-10 17:10:01 +0700

    add update function updataeclass

---
+++ src/UpdateData.java
@@ -0,0 +1,34 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.text.AbstractDocument.LeafElement;
+
+import com.mysql.jdbc.Connection;
+
+public class UpdateData {
+
+	public void LetUp(String cmd, String status, String id) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/pingpong", "root", "");
+
+			Statement s = null;
+			s = conn.createStatement();
+
+			if (s.execute("UPDATE user SET sick_type='" + status + "',Commend='" + cmd + "' WHERE id = " + id)) {
+
+			} else {
+				System.out.println("ä´éÅÐ´Í¡¡¡¡¡¡¡");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
