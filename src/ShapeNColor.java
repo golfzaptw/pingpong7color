a49b52d3991ef4d00b9cf21af23e4d079d1a9286
fluke <flukebevil@gmail.com>
2017-05-10 18:38:01 +0700

    repare color class v1

--- src/ShapeNColor.java
+++ src/ShapeNColor.java
@@ -10,12 +10,12 @@ import Color.*;
 import Factory.*;
 
 public class ShapeNColor {
-	public void GetColor_sick(int sickType ,JFrame frame,int LocationX , int LocationY) {
+	public JPanel GetColor_sick(int sickType ,JFrame frame,int LocationX , int LocationY) {
 
 		AbstractFactory color = FactoryProducer.getFactory("Color");
 		JPanel Color_OMG = color.getColor(sickType);
 		Color_OMG.setLocation(LocationX, LocationY);
 		Color_OMG.setSize(250, 250);
-		frame.add(Color_OMG);
+		return Color_OMG;
 	}
 }
